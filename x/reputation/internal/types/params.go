2b48a740afa8161db56cfc4926ce94e8fae5ee8b
Emily Williams <emily.williams@levelk.io>
2020-01-31 12:32:10 -0500

    /types/ directory boilerplate

---
+++ x/reputation/internal/types/params.go
@@ -0,0 +1,13 @@
+package types
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/x/params"
+)
+
+// Default parameter namespace
+const (
+	DefaultParamspace = ModuleName
+	// TODO: Define your default parameters
+)
